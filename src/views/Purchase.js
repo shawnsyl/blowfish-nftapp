1c5c0d706a6b9297cac02b69df7bf9184ab43ccc
Shawn Lee <eesy96@gmail.com>
2021-04-25 03:59:42 -0600

    add dumy purchase copywriting

--- src/views/Purchase.js
+++ src/views/Purchase.js
@@ -16,7 +16,7 @@ const Purchase = props => {
     } = useContractDataContext();
 
     return (
-        <section className='section container'>
+        <section className='purchase section container'>
             {!!web3 && !!contract & !reloadRequired ? (
                 <Exchange />
             ) : (
@@ -25,6 +25,9 @@ const Purchase = props => {
                     </div>
                 )
             }
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
         </section>
     )
 }
